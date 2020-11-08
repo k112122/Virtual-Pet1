b58636dda733a9831951f10fa598a390c56010b9
k112122 <70462311+k112122@users.noreply.github.com>
2020-11-08 10:43:34 +0530

    Add files via upload

---
+++ Virtual-Pet--1-master/sketch.js
@@ -0,0 +1,57 @@
+var dog, happyDog, foodS, foodStock, database;
+function preload()
+{
+dogImg= loadImage("images/dogImg.png");
+happyDogImg= loadImage("images/dogImg1.png");
+
+}
+
+function setup() {
+  createCanvas(500, 500);
+  database = firebase.database();
+  dog = createSprite(250, 450);
+  dog.addImage(dogImg);
+  dog.scale = 0.2;
+  foodStock=database.ref('Food');
+  foodStock.on("value", readStock);
+}
+
+
+function draw() {  
+background(rgb(46, 139, 87));
+
+if(keyWentDown(UP_ARROW)){
+writeStock(foodS);
+dog.addImage(happyDogImg);
+
+}
+  drawSprites();
+stroke (10);
+  fill ("white");
+text ("Food: "+foodS, 400, 100);
+
+}
+
+
+function readStock(data){
+foodS=data.val();
+
+}
+function writeStock(x){
+  if(x<=0){
+
+x=0;
+
+
+
+  }
+  else{
+
+    x=x-1;
+  }
+database.ref('/').update({
+  Food:x
+})
+
+
+}
